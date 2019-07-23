8cf6623e9f83d5ad5ece18a543275b29f353432c
Serene Deng <dengxiaopu@gmail.com>
2019-07-23 05:06:34 -0700

    Structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,56 @@
-alert('hello');
+
+var time = 3;
+
+$('#start').click(function(){
+	$('#start').hide();
+	$('#form').fadeIn();
+	$('#counter').fadeIn();
+
+
+$('#counter').text(time);
+function count(){
+	time--;
+	$('#counter').text(time);
+
+	if(time==-1){
+		clearInterval(timeOut);
+		$('#form').hide();
+		$('#counter').hide();
+		$('#statistic').fadeIn();
+		$('#try').fadeIn();
+
+	}
+}
+var timeOut = setInterval(count, 1*1000);
+
+});
+
+$('#try').click(function(){
+	$('#try').hide();
+	$('#statistic').hide();
+	$('#form').fadeIn();
+	$('#counter').fadeIn();
+
+time = 3;
+$('#counter').text(time);
+function count(){
+	time--;
+	$('#counter').text(time);
+
+	if(time==-1){
+		clearInterval(timeOut);
+		$('#form').hide();
+		$('#counter').hide();
+		$('#statistic').fadeIn();
+		$('#try').fadeIn();
+
+	}
+}
+var timeOut = setInterval(count, 1*1000);
+
+});
+
+
+
+
+
