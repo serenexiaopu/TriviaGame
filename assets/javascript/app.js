7fe9f3fba04a7b174cf4f9429cd4e537ef206e5d
Serene Deng <dengxiaopu@gmail.com>
2019-07-29 09:05:21 -0700

    Fix a bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
-
-var time = 10;
+var time = 30;
 var trueAn = 0;
 var falAn = 0;
 var unAn = 0;
@@ -10,124 +9,129 @@ $('#wrong').text(falAn);
 $('#unanswered').text(unAn);
 
 //-----Counter
-$('#start').click(function(){
-	$('#start').hide();
-	$('#form').fadeIn();
-	$('#counter').fadeIn();
-	$('input').fadeIn();
-	$('#counter').text(time);
+$('#start').click(function() {
+    $('#start').hide();
+    $('#form').fadeIn();
+    $('#counter').fadeIn();
+    $('input').fadeIn();
+    $('#counter').text(time);
 
 });
 //------count function	
-	function count(){
-		time--;
-		$('#counter').text(time);
+function count() {
+    time--;
+    $('#counter').text(time);
 
-		if(time==-1){
-			clearInterval(timeOut);
-			$('#form').hide();
-			$('#counter').hide();
-			$('input').hide();
-			$('#statistic').fadeIn();
-			$('#try').fadeIn();
+    if (time == -1) {
+        clearInterval(timeOut);
+        $('#form').hide();
+        $('#counter').hide();
+        $('input').hide();
+        $('#statistic').fadeIn();
+        $('#try').fadeIn();
 
-			submitForms;
+        submitForms;
 
-}     
+    }
 }
 
-var timeOut = setInterval(count, 1*1000);
+var timeOut = setInterval(count, 1 * 1000);
 
-submitForms = function(event){
-  // event.preventDefault();
+submitForms = function(event) {
+    // event.preventDefault();
 
-   $('#form').hide();
-				$('#counter').hide();
-				$('input').hide();
-				$('#statistic').fadeIn();
-				$('#try').fadeIn();
+    $('#form').hide();
+    $('#counter').hide();
+    $('input').hide();
+    $('#statistic').fadeIn();
+    $('#try').fadeIn();
 
-   // $('#form1').submit();
-   // $('#form2').submit();
-   // $('#form3').submit();
-   // $('#form4').submit(); 
+    // $('#form1').submit();
+    // $('#form2').submit();
+    // $('#form3').submit();
+    // $('#form4').submit(); 
 
-   
+
 }
 
 
 
- $('input').click(function(){
+$('input').click(function() {
 
-    	var chooseVal = '';
+    var chooseVal = '';
 
-			chooseVal = $(this).val();
-			console.log(chooseVal);
-			console.log(this);
+    chooseVal = $(this).val();
+    console.log(chooseVal);
+    console.log(this);
 
-		
-			if(chooseVal == "1"){
-				trueAn++;
-				$('#correct').text(trueAn);
 
-			}
-			
-			if(chooseVal == "2"){
-				falAn++;
-				$('#wrong').text(falAn);
+    if (chooseVal == "1") {
+        trueAn++;
+        $('#correct').text(trueAn);
 
-			}if(chooseVal == ""){
-				unAn++;
-				$('#unanswered').text(unAn);
-			}
+    }
 
-		});
+    if (chooseVal == "2") {
+        falAn++;
+        $('#wrong').text(falAn);
 
- 	// debugger;
-			
-		
+    }
+    if (chooseVal == "") {
+        unAn++;
+        $('#unanswered').text(unAn);
+    }
+
+});
+
+// debugger;
+
+
 
 
 
 //----start again
-$('#try').click(function(){
-	$('#try').hide();
-	$('#statistic').hide();
-	$('#form').fadeIn();
-	$('#counter').fadeIn();
+$('#try').click(function() {
+    $('#try').hide();
+    $('#statistic').hide();
+    $('#start').hide();
+    $('#form').fadeIn();
+    $('#counter').fadeIn();
+    $('input').fadeIn();
+    $('#counter').text(time);
 
-	time = 5;
-	trueAn = 0;
+
+    time = 30;
+    trueAn = 0;
     falAn = 0;
     unAn = 0;
 
-     $('input[type=radio]').each(function() 
-{ 
-        this.checked = false; 
-}); 
+    $('input[type=radio]').each(function() {
+        this.checked = false;
+    });
 
     $('#correct').text(trueAn);
-	$('#wrong').text(falAn);
-	$('#unanswered').text(unAn);
+    $('#wrong').text(falAn);
+    $('#unanswered').text(unAn);
 
-   
 
-	$('#counter').text(time);
-	function count(){
-		time--;
-		$('#counter').text(time);
 
-		if(time==-1){
-			clearInterval(timeOut);
-		$('#form').hide();
-		$('#counter').hide();
-		$('#statistic').fadeIn();
-		$('#try').fadeIn();
-		$('#wrong').text(falAn);
+    $('#counter').text(time);
 
-		}
-	}
-	var timeOut = setInterval(count, 1*1000);
+    function count() {
+        time--;
+        $('#counter').text(time);
+
+        if (time == -1) {
+            clearInterval(timeOut);
+            $('#form').hide();
+            $('#counter').hide();
+            $('#statistic').fadeIn();
+            $('#try').fadeIn();
+            $('#wrong').text(falAn);
+
+        }
+    }
+    var timeOut = setInterval(count, 1 * 1000);
 
 
 
